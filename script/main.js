8bdbdf34239067af9916beb8c2452ca763d7c161
Oleh Starodubets <stilixoff@gmail.com>
2020-03-15 17:34:14 +0200

    ашч bug

--- script/main.js
+++ script/main.js
@@ -1,36 +1,31 @@
-let getNameFilm = document.getElementById('getFilm');
+// let getNameFilm = document.getElementById('getFilm');
 
-
-
-
-
-
-    getNameFilm.onclick = function(){
-        if(document.getElementById('films').value != 0) {
-            // document.getElementById('filmPage').classList.toggle('active');
-            document.getElementById('loader').classList.toggle('active');
-            let idFilm = document.getElementById('films').value;
-            axios.get(`https://swapi.co/api/films/${idFilm}`)
-            .then((res) => {
-                document.getElementById('idFilm').innerHTML = `${res.data.episode_id}`;
-                document.getElementById('nameFilm').innerHTML = `${res.data.title}`;
-                console.log(res);
-                return res;
-            }).then((res) => {
-                for (let index = 0; index < res.data.characters.length; index++) {
-                    axios.get(res.data.characters[index])
-                    .then((res) => {
-                        console.log(res.data.name);
-                    })
+//     getNameFilm.onclick = function(){
+//         if(document.getElementById('films').value != 0) {
+//             // document.getElementById('filmPage').classList.toggle('active');
+//             document.getElementById('loader').classList.toggle('active');
+//             let idFilm = document.getElementById('films').value;
+//             axios.get(`https://swapi.co/api/films/${idFilm}`)
+//             .then((res) => {
+//                 document.getElementById('idFilm').innerHTML = `${res.data.episode_id}`;
+//                 document.getElementById('nameFilm').innerHTML = `${res.data.title}`;
+//                 console.log(res);
+//                 return res;
+//             }).then((res) => {
+//                 for (let index = 0; index < res.data.characters.length; index++) {
+//                     axios.get(res.data.characters[index])
+//                     .then((res) => {
+//                         console.log(res.data.name);
+//                     })
                     
-                }
-                console.log(res);
-            }).finally(() => {
-                // document.getElementById('filmPage').classList.toggle('active');
-                document.getElementById('loader').classList.toggle('active');
-            });
-        }
-    }
+//                 }
+//                 console.log(res);
+//             }).finally(() => {
+//                 // document.getElementById('filmPage').classList.toggle('active');
+//                 document.getElementById('loader').classList.toggle('active');
+//             });
+//         }
+//     }
 
 // getNameFilm.onclick = function(){
 //     // document.getElementById('filmPage').classList.toggle('active');
@@ -56,3 +51,36 @@ let getNameFilm = document.getElementById('getFilm');
 //                         .then((res) => {
 //                             console.log(res.data);
 //                         });
+
+
+
+let getNameFilm = document.getElementById('getFilm');
+    getNameFilm.onclick = function(){
+        if(document.getElementById('films').value != 0) {
+            // document.getElementById('filmPage').classList.toggle('active');
+            document.getElementById('loader').classList.toggle('active');
+            let idFilm = document.getElementById('films').value;
+            axios.get(`https://swapi.co/api/films/${idFilm}`)
+            .then((res) => {
+                document.getElementById('idFilm').innerHTML = `${res.data.episode_id}`;
+                document.getElementById('nameFilm').innerHTML = `${res.data.title}`;
+                console.log(res);
+                return res;
+            }).then((res) => {
+                let promiseArr = [];
+                for (let index = 0; index < res.data.characters.length; index++) {
+                    let promise = axios.get(res.data.characters[index])
+                    .then((res) => {
+                        console.log(res.data.name);
+                        return res.data.name;
+                    })
+                    promiseArr.push(promise);
+                }
+                console.log(res);
+                return Promise.all(promiseArr);
+            }).finally(() => {
+                // document.getElementById('filmPage').classList.toggle('active');
+                document.getElementById('loader').classList.toggle('active');
+            });
+        }
+    }

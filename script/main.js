511911f4ded16f54732b0f725cace79b22b62da0
Oleh Starodubets <stilixoff@gmail.com>
2020-03-16 14:16:56 +0200

    done index.html

--- script/main.js
+++ script/main.js
@@ -7,20 +7,24 @@
 
 
 
+document.getElementById('filmPage').classList.toggle('active');
 let getNameFilm = document.getElementById('getFilm');
     getNameFilm.onclick = function(){
         if(document.getElementById('films').value != 0) {
-            // document.getElementById('filmPage').classList.toggle('active');
+            document.getElementById('filmPage').classList.add('active');
             document.getElementById('loader').classList.toggle('active');
             let idFilm = document.getElementById('films').value;
             axios.get(`https://swapi.co/api/films/${idFilm}`)
             .then((res) => {
                 document.getElementById('idFilm').innerHTML = `${res.data.episode_id}`;
                 document.getElementById('nameFilm').innerHTML = `${res.data.title}`;
+                document.getElementById('filmInfo').innerHTML = `${res.data.opening_crawl}`;
+                document.getElementById('imgFilm').innerHTML = `<img src="./img/${res.data.title}.jpg" alt="${res.data.title}">`;
                 console.log(res);
                 return res;
             }).then((res) => {
                 let heroes = document.getElementById('heroes');
+                heroes.innerHTML = '';
                 let promiseArr = [];
                 for (let index = 0; index < res.data.characters.length; index++) {
                     let promise = axios.get(res.data.characters[index])
@@ -37,16 +41,18 @@ let getNameFilm = document.getElementById('getFilm');
                         </ul>
                     </div>
                         `
-                        console.log(res.data);
+                        console.log(res.data.name);
                         return res.data.name;
                     })
                     promiseArr.push(promise);
                 }
                 console.log(res);
                 return Promise.all(promiseArr);
+            }).catch((err) => {
+                document.getElementById('filmPage').innerHTML = `<h5>Что то пошло не так</h5> <br> ${err}`
             }).finally(() => {
-                // document.getElementById('filmPage').classList.toggle('active');
+                document.getElementById('filmPage').classList.remove('active');
                 document.getElementById('loader').classList.toggle('active');
             });
-        }
+}
     }
